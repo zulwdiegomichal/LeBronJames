51ed7411b14e73e2b8e0836663e7fbd53d402424
zulwdiegomichal <michalb1287@gmail.com>
2025-03-26 16:08:21 +0100

    nic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import './App.css'
 
 function App() {
+const komp=2;
+console.log(komp);
+let b=244+5542;
+console.log(b);
+let y=10;
+let n=komp*3+2*y-komp;
+console.log(n);
 
-  return <>Hello, World!</>
+
+
+
+  return <>Hello, World!
+<p className='test'>rownanie={b}</p>  
+ <p> Moj komputer ma numer {komp} </p>
+<p> rownanie={n}</p>
+
+<div> 
+{n}  
+
+
+</div>
+
+
+  </>
+
 
   
 }
