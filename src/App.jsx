b906af88d675f386f247f1820f7ddecdd45b516d
zulwdiegomichal <michalb1287@gmail.com>
2025-04-24 11:31:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ let b=244+5542;
 console.log(b);
 let y=10;
 let n=komp*3+2*y-komp;
+let w1=komp*1
+
+
 console.log(n);
 
 
@@ -17,9 +20,17 @@ console.log(n);
  <p> Moj komputer ma numer {komp} </p>
 <p> rownanie={n}</p>
 
-<div> 
-{n}  
-
+<div className='mnozenie'> 
+<p> Tablica mnozenia</p>
+<p> {komp}*1={w1} </p>  
+<p> {komp}*2={w2} </p>  
+<p> {komp}*3={w3} </p>  
+<p> {komp}*4={w4} </p>  
+<p> {komp}*5={w5} </p>  
+<p> {komp}*6={w6} </p>
+<p> {komp}*7={w7} </p>  
+<p> {komp}*8={w8} </p>  
+<p> {komp}*9={w9} </p>  
 
 </div>
 
